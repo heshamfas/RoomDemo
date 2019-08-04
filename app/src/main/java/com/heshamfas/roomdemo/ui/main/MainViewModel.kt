9a1161c092faf51564a98e6e2bc60a97b5a2c5e6
Hesham Fas <HeshamFas@outlook.com>
2019-08-04 18:53:13 -0500

    adding view model

--- app/src/main/java/com/heshamfas/roomdemo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/heshamfas/roomdemo/ui/main/MainViewModel.kt
@@ -1,7 +1,36 @@
 package com.heshamfas.roomdemo.ui.main
 
-import android.arch.lifecycle.ViewModel
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import com.heshamfas.roomdemo.Product
+import com.heshamfas.roomdemo.ProductRepository
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     // TODO: Implement the ViewModel
+    private val repository: ProductRepository = ProductRepository(application)
+    private val allProducts: LiveData<List<Product>>?
+    private val searchResults:MutableLiveData<List<Product>>
+
+    init {
+        allProducts = repository.allProducts
+        searchResults = repository.searchResults
+    }
+
+    fun insertProdcut(name:String){
+        repository.findProduct(name)
+    }
+    fun deleteProduct(name:String){
+        repository.deleteProduct(name)
+    }
+
+    fun getSearchResults():MutableLiveData<List<Product>>{
+        return searchResults
+    }
+
+    fun getAllProducts():LiveData<List<Product>>?{
+        return allProducts
+    }
+
 }
